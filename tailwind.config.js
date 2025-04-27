37d4a91b93b52f4bf75b58e66e9f107f59a1d9d0
Mik-kunin-19 <122996070+Mik-kunin-19@users.noreply.github.com>
2025-04-27 17:59:51 +0200

    created tailwind.config.js
    colorscheme definitions

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        darkPurple: '#2D033B',
+        emerald: '#00FFAB',
+        softWhite: '#F7F7F7',
+      },
+    },
+  },
+  plugins: [],
+}
